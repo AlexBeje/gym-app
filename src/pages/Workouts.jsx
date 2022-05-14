bf22cdaed9743fd3a5c465dbbe20c037c170a9ef
AlexBeje <54982557+AlexBeje@users.noreply.github.com>
2022-05-14 09:04:19 +0200

    🐛 FIX: Fixed mobile padding issue.

--- src/pages/Workouts.jsx
+++ src/pages/Workouts.jsx
@@ -5,13 +5,23 @@ function Workouts() {
   return (
     <div className="bg-light-100 h-full rounded-t-2xl text-dark-900">
       <div className="flex h-full overflow-x-auto p-6 relative snap-mandatory snap-x w-full gap-3">
-        {workoutsData && workoutsData.map((workout, index) => {
-          return (
-            <div className="flex-none snap-center w-full" key={index}>
-              <Workout title={workout.title} exercisesData={workout.exerecises} />
-            </div>
-          );
-        })}
+        {workoutsData &&
+          workoutsData.map((workout, index) => {
+            return (
+              <div
+                className={`flex-none snap-center w-full ${
+                  workout.lastWorkout && "pr-6"
+                }`}
+                key={index}
+              >
+                <Workout
+                  title={workout.title}
+                  exercisesData={workout.exerecises}
+                  lastWorkout={workout.lastWorkout}
+                />
+              </div>
+            );
+          })}
       </div>
     </div>
   );
