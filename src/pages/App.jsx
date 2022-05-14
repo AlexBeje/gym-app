633f19f42a5be2b13df738ca8c475783f544ec8a
AlexBeje <54982557+AlexBeje@users.noreply.github.com>
2022-05-14 19:33:00 +0200

    👌 IMPROVE: Updated the structure of the app file.

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,11 +1,27 @@
-import Header from "../components/Header";
-import Workouts from "./Workouts";
+import workoutsData from "../../db/workouts";
+import Workout from "../components/Workout";
 
 function App() {
   return (
     <div className="bg-dark-900 flex flex-col h-screen text-light-100">
-      <Header title="Workouts"/>
-      <Workouts />
+      <h1 className="font-bold text-3xl text-center w-full p-3">Workouts</h1>
+      <div className="bg-light-100 h-full rounded-t-2xl text-dark-900">
+        <div className="flex h-full overflow-x-auto p-6 snap-mandatory snap-x gap-3">
+          {workoutsData &&
+            workoutsData.map((workout, index) => {
+              return (
+                <div className="flex-none snap-center w-full" key={index}>
+                  <Workout
+                    title={workout.title}
+                    exercisesData={workout.exerecises}
+                    video={workout.video}
+                  />
+                </div>
+              );
+            })}
+          <div className="min-w-[24px]" />
+        </div>
+      </div>
     </div>
   );
 }
