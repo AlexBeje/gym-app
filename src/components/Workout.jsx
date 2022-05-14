419762223b2807a3c23f0f29568b85b4236c8810
AlexBeje <54982557+AlexBeje@users.noreply.github.com>
2022-05-14 18:45:10 +0200

    👌 IMPROVE: Updated the icon position of the video.

--- src/components/Workout.jsx
+++ src/components/Workout.jsx
@@ -6,18 +6,9 @@ function Workout({ title, video, exercisesData }) {
     <div className="bg-dark-900 flex flex-col h-full relative rounded-xl shadow-lg shadow-light-900 text-light-100">
       <div className="absolute bg-gradient-to-t bottom-0 from-dark-800 h-full rounded-b-lg to-transparent w-full" />
       <div className="flex flex-col h-full">
-        <div className="flex items-center justify-between">
-          <div className="min-w-[20px]" />
-          <h1 className="flex font-bold items-center justify-center min-h-[64px] text-2xl text-center z-10">
-            {title}
-          </h1>
-          <a
-            href={video}
-            className="z-20 mr-10"
-          >
-            <IoVideocam className="text-dark-300" size="24" />
-          </a>
-        </div>
+        <h1 className="flex font-bold items-center justify-center min-h-[64px] text-2xl text-center z-10">
+          {title}
+        </h1>
         <div className="flex flex-col h-full z-10">
           {exercisesData &&
             exercisesData.map((exercise, index) => {
@@ -30,7 +21,11 @@ function Workout({ title, video, exercisesData }) {
               );
             })}
         </div>
-        <div className="min-h-[64px]" />
+        <div className="flex items-center justify-center min-h-[64px]">
+          <a href={video} className="z-20">
+            <IoVideocam className="text-dark-300" size="24" />
+          </a>
+        </div>
       </div>
     </div>
   );
