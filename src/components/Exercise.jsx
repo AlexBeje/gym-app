6801a6deabd3f99ed0700ea0e1e04a8152224c1c
AlexBeje <54982557+AlexBeje@users.noreply.github.com>
2022-05-14 10:08:47 +0200

    👌 IMPROVE: Updated the exercises layout and added more workout data.

--- src/components/Exercise.jsx
+++ src/components/Exercise.jsx
@@ -4,36 +4,61 @@ function Exercise({ exerciseData, firstExercise }) {
   const [checked, setChecked] = useState(false);
 
   return (
-    <div
-      className={`px-6 py-3 
+    <div className="flex">
+      { exerciseData.superSet && !checked && <div className="bg-dark-500 min-w-[8px] z-10"/>}
+      <div
+        className={`opacity-80 px-6 py-3 w-full
         ${firstExercise ? "border-y-[1px]" : "border-b-[1px]"}
         ${checked ? "border-green-600" : "border-dark-600"}
-        ${checked ? "bg-green-500 text-dark-900" : "bg-dark-700"}`}
-      onClick={() => setChecked(!checked)}
-    >
-      <h2 className="font-bold mb-2 text-2xl">{exerciseData.name}</h2>
-      <div className="flex justify-between">
-        <div className="flex gap-2 items-center">
-          <p className="flex items-center">
-            <span className="font-bold mr-2 text-2xl">{exerciseData.sets}</span>{" "}
-            sets
-          </p>
-          /
-          <p className="flex items-center">
-            <span className="font-bold mr-2 text-2xl">{exerciseData.reps}</span>{" "}
-            reps
-          </p>{" "}
-        </div>
-        <div className="flex items-center">
-          <input
-            type="text"
-            defaultValue={exerciseData.weight}
-            onClick={(e) => e.stopPropagation()}
-            className={`flex mr-2 p-2 rounded-sm text-center w-11
-              ${checked ? "bg-green-600 text-dark-900" : "bg-dark-800"}
+        ${
+          checked ? "bg-green-500 text-dark-900" : "bg-dark-700 text-dark-100"
+        }`}
+        onClick={() => setChecked(!checked)}
+      >
+        <h2 className="font-bold mb-1 text-xl">{exerciseData.name}</h2>
+        <div className="flex justify-between">
+          <div className="flex gap-2 items-center">
+            <p className="flex items-center">
+              <span
+                className={`font-bold mr-2 text-2xl 
+              ${checked ? "text-dark-900" : "text-light-100"}`}
+              >
+                {exerciseData.sets}
+              </span>
+              sets
+            </p>
+            {!exerciseData.editableReps && <span>/</span>}
+            {!exerciseData.editableReps && (
+              <p className="flex items-center">
+                <span
+                  className={`font-bold mr-2 text-2xl 
+                ${checked ? "text-dark-900" : "text-light-100"}`}
+                >
+                  {exerciseData.reps}
+                </span>
+                reps
+              </p>
+            )}
+          </div>
+          <div className="flex items-center">
+            {!exerciseData.editableReps ? <span>kg</span> : <span>reps</span>}
+            <input
+              type="text"
+              defaultValue={
+                exerciseData.editableReps
+                  ? exerciseData.editableReps
+                  : exerciseData.weight
+              }
+              onClick={(e) => e.stopPropagation()}
+              className={`flex ml-2 p-2 rounded-sm text-center font-bold w-12
+              ${
+                checked
+                  ? "bg-green-600 text-dark-900"
+                  : "bg-dark-800 text-light-100"
+              }
             `}
-          />
-          kg
+            />
+          </div>
         </div>
       </div>
     </div>
--- src/components/Workout.jsx
+++ src/components/Workout.jsx
@@ -4,7 +4,7 @@ function Workout({ title, exercisesData }) {
   return (
     <div className="bg-dark-900 flex flex-col h-full relative rounded-xl shadow-lg shadow-light-900 text-light-100">
       <div className="absolute bg-gradient-to-b from-dark-800 h-3/6 rounded-t-lg to-transparent w-full" />
-      <h1 className="font-bold p-6 text-3xl text-center z-10">{title}</h1>
+      <h1 className="font-bold p-4 text-2xl text-center z-10">{title}</h1>
       <div className="z-10">
         {exercisesData && exercisesData.map((exercise, index) => {
           return (
