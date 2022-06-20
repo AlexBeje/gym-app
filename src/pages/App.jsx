cf5a86e48d4eb2ad61a3d060a88122d9e8ff8d1c
AlexBeje <54982557+AlexBeje@users.noreply.github.com>
2022-06-20 17:28:51 +0200

    📦 NEW: Updated week icon and added week counter.

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,6 +1,9 @@
 // React
 import { useState } from "react";
 
+// Use Hooks TS
+import { useLocalStorage } from "usehooks-ts";
+
 // Data
 import workoutsData from "../../db/workouts";
 
@@ -8,23 +11,36 @@ import workoutsData from "../../db/workouts";
 import Workout from "../components/Workout";
 
 // Icons
-import { AiOutlineReload } from "react-icons/ai";
+import { IoBarbell } from "react-icons/io5";
+
+// Constants
+const WEEK_COUNTER_DEFAULT = 1;
 
 function App() {
   const [resetExercise, setResetExercise] = useState(false);
+  const [weekCounter, setWeekCounter] = useLocalStorage("weekCounter", WEEK_COUNTER_DEFAULT);
 
   const handleResetClick = () => {
+    setWeekCounter(weekCounter + 1);
     setResetExercise(true);
+  };
+
+  const handleWeekClick = () => {
+    if(weekCounter > 1) {
+      setWeekCounter(weekCounter - 1);
+    }
   };
 
   return (
     <div className="bg-dark-900 flex flex-col h-screen text-light-100">
       <div className="flex">
-        <div className="flex-1 min-w-[20px] ml-8" />
+        <div className="flex flex-1 font-bold items-center justify-center min-w-[30px] ml-8 text-dark-200 text-xl">
+          <span onClick={handleWeekClick}>{weekCounter}</span>
+        </div>
         <h1 className="font-bold p-3 text-3xl text-center w-full">Workouts</h1>
         <div className="flex flex-1 items-center justify-end">
-          <AiOutlineReload
-            className="cursor-pointer mr-8 text-dark-200 text-xl"
+          <IoBarbell
+            className="cursor-pointer mr-8 text-3xl text-dark-200"
             onClick={handleResetClick}
           />
         </div>
